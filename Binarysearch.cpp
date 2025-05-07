80efea72c0ea4ca7b338f1a211dc450eb8238378
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-05-07 19:55:37 +0700

    Binary search 6

--- Binarysearch.cpp
+++ Binarysearch.cpp
@@ -39,7 +39,12 @@ void bubbleSortArray()
         for (int j = 0; j <= nPanjang - 1 - pass; j++)
         {
             if (element[j] > element [j + 1])
+            {
+                int temp = element[j];
+                element[j] = element[j + 1];
+                element[j + 1] = temp;
+            }
         }
-    } 
-    
+        pass++;
+    } while (pass <= nPanjang - 1);
 }
